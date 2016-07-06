cd8232b6614d6bb18849514eff06be540283a205
rowan self <rowan.self@gmail.com.com>
2016-07-06 17:36:07 +1200

    added model code for routes and stoptimes

--- app/src/main/java/com/example/maria/offlinemetlinkapp/model/routes.java
+++ app/src/main/java/com/example/maria/offlinemetlinkapp/model/routes.java
@@ -4,4 +4,98 @@ package com.example.maria.offlinemetlinkapp.model;
  * Created by bobl on 7/6/16.
  */
 public class routes {
+    /*`route_id` text,
+            `agency_id` text,
+            `route_short_name` text,
+            `route_long_name` text,
+            `route_desc` text,
+            `route_type` int(11)
+            `route_url` text,
+             `route_color` text,
+            `route_text_color` text
+            */
+    String routeId;
+    String routeShortName;
+    String routeLongName;
+    String routeDesc;
+    int routeType;
+    String routeUrl;
+    String routeColor;
+    String routeTextColor;
+    public routes(String route_id, String route_short_name, String route_long_name, String route_desc,int route_type, String route_url,String route_color, String route_text_color){
+        routeId = route_id;
+        routeShortName = route_short_name;
+        routeLongName = route_long_name;
+        routeDesc = route_desc;
+        routeType = route_type;
+        routeUrl = route_url;
+        routeColor = route_color;
+        routeTextColor = route_text_color;
+    }
+    //------getter----------------
+    public String getRouteId() {
+        return routeId;
+    }
+
+    public String getRouteShortName() {
+        return routeShortName;
+    }
+
+    public String getRouteLongName() {
+        return routeLongName;
+    }
+
+    public String getRouteDesc() {
+        return routeDesc;
+    }
+
+    public int getRouteType() {
+        return routeType;
+    }
+
+    public String getRouteUrl() {
+        return routeUrl;
+    }
+
+    public String getRouteColor() {
+        return routeColor;
+    }
+
+    public String getRouteTextColor() {
+        return routeTextColor;
+    }
+    //------setter----------------
+
+
+    public void setRouteId(String routeId) {
+        this.routeId = routeId;
+    }
+
+    public void setRouteShortName(String routeShortName) {
+        this.routeShortName = routeShortName;
+    }
+
+    public void setRouteLongName(String routeLongName) {
+        this.routeLongName = routeLongName;
+    }
+
+    public void setRouteDesc(String routeDesc) {
+        this.routeDesc = routeDesc;
+    }
+
+    public void setRouteType(int routeType) {
+        this.routeType = routeType;
+    }
+
+    public void setRouteUrl(String routeUrl) {
+        this.routeUrl = routeUrl;
+    }
+
+    public void setRouteColor(String routeColor) {
+        this.routeColor = routeColor;
+    }
+
+    public void setRouteTextColor(String routeTextColor) {
+        this.routeTextColor = routeTextColor;
+    }
 }
--- app/src/main/java/com/example/maria/offlinemetlinkapp/model/stop_times.java
+++ app/src/main/java/com/example/maria/offlinemetlinkapp/model/stop_times.java
@@ -4,4 +4,53 @@ package com.example.maria.offlinemetlinkapp.model;
  * Created by bobl on 7/6/16.
  */
 public class stop_times {
+    /*
+   `trip_id` text,
+  `arrival_time` text,
+  `departure_time` text,
+  `stop_id` int(11) DEFAULT NULL
+
+    * */
+    String tripId;
+    String arrivalTime;
+    String departureTime;
+    int stopId;
+    public stop_times(String trip_id,String arrival_time,String departure_time,int stop_id){
+        tripId = trip_id;
+        arrivalTime = arrival_time;
+        departureTime = departure_time;
+        stopId = stop_id;
+    }
+    //------setters---------------------
+    public void setTripId(String tripId) {
+        this.tripId = tripId;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public void setStopId(int stopId) {
+        this.stopId = stopId;
+    }
+    //------getters---------------------
+    public String getTripId() {
+        return tripId;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public int getStopId() {
+        return stopId;
+    }
 }
