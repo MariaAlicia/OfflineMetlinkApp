e604878ca3703446a38fcb8f6f280906342e080a
mgoo <andrewmcghienz@gmail.com>
2016-07-06 16:27:29 +1200

    added model package

--- app/src/main/java/com/example/maria/offlinemetlinkapp/helper/DatabaseHelper.java
+++ app/src/main/java/com/example/maria/offlinemetlinkapp/helper/DatabaseHelper.java
@@ -1,7 +1,15 @@
 package com.example.maria.offlinemetlinkapp.helper;
 
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by mgoo on 6/07/16.
  */
-public class DatabaseHelper {
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String LOG = "DatabaseHelper";
+
+    private static final int DATABASE_VERSION = 1;
+
+    //private
 }
