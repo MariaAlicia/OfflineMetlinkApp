51e04f4a45e6d70fc771cbf1ad8d6c06d5d7a5af
Sami <puggsoy@gmail.com>
2016-07-18 12:53:51 +1200

    Added keyboard hiding for stop input

--- app/src/main/java/com/example/maria/offlinemetlinkapp/MainActivity.java
+++ app/src/main/java/com/example/maria/offlinemetlinkapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.maria.offlinemetlinkapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -31,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 	    stopInput = (EditText)findViewById(R.id.stopInput);
 	    routeSpinner = (Spinner)findViewById(R.id.routeSpinner);
 	    setupListeners();
+	    setupKeyboardHide(findViewById(R.id.parentLayout));
     }
 
 	private void setupListeners(){
@@ -112,6 +117,38 @@ public class MainActivity extends AppCompatActivity {
         //DataController dataController = new DataController();
         //dataController.updateDatabase(getAssets());
     }
+
+	public static void hideSoftKeyboard(Activity activity) {
+		InputMethodManager inputMethodManager = (InputMethodManager)  activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+		inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+	}
+
+	public void setupKeyboardHide(View view) {
+
+		//Set up touch listener for non-text box views to hide keyboard.
+		if(!(view instanceof EditText)) {
+
+			view.setOnTouchListener(new View.OnTouchListener() {
+
+				public boolean onTouch(View v, MotionEvent event) {
+					hideSoftKeyboard(MainActivity.this);
+					return false;
+				}
+
+			});
+		}
+
+		//If a layout container, iterate over children and seed recursion.
+		if (view instanceof ViewGroup) {
+
+			for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+
+				View innerView = ((ViewGroup) view).getChildAt(i);
+
+				setupKeyboardHide(innerView);
+			}
+		}
+	}
 
     public ArrayList getRoutes(){
         return new ArrayList(8);
