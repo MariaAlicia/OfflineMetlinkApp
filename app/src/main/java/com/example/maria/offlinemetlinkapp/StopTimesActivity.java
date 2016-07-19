5b35fe7c2218dc92e870cb5dfbf99488b7fb55ac
Sami <puggsoy@gmail.com>
2016-07-19 19:28:41 +1200

    Make the Up button do the same as the back button

--- app/src/main/java/com/example/maria/offlinemetlinkapp/StopTimesActivity.java
+++ app/src/main/java/com/example/maria/offlinemetlinkapp/StopTimesActivity.java
@@ -2,6 +2,7 @@ package com.example.maria.offlinemetlinkapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -35,5 +36,17 @@ public class StopTimesActivity extends AppCompatActivity {
 				Toast.makeText(StopTimesActivity.this, mainListView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
 			}
 		});
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId())
+		{
+			case android.R.id.home:
+				onBackPressed();
+				return true;
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 }
